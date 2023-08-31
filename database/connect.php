0f6fdabf173c556b7152da0b15d5d4ce3b104ade
Damian Silvera <damiansebastians@gmail.com>
2023-08-31 16:34:14 +0200

    add config

--- database/connect.php
+++ database/connect.php
@@ -2,20 +2,44 @@
 
 require_once "../db-config.php";
 
-    $host = constant("DB_HOST");
-    $user = constant("DB_USER");
-    $password = constant("DB_PASSWORD");
-    $database = constant("DB_DATABASE");
+$host = constant("DB_HOST");
+$user = constant("DB_USER");
+$password = constant("DB_PASSWORD");
+$database = constant("DB_DATABASE");
 
-        $conn = new mysqli($host, $user, $password, $database);
-        
-        if ($conn->connect_error) {
-            die("Connection failed. Error: " . $conn->connect_error);
-        }
 
-        // QUERY
-        $query = "SELECT * FROM tabla";
-        $result = $conn->query($query);
+//Create connection
+$conn = new mysqli($host, $user, $password, $database);
 
-        // CLOSE CONNECTION
-        $conn->close();
+if ($conn->connect_error) {
+    die("Connection failed. Error: " . $conn->connect_error);
+}
+echo "Connected to database successfully";
+
+//Query
+$query = "SELECT * FROM bookings";
+$result = $conn->query($query);
+
+echo $result;
+
+//close connection
+$conn->close();
+
+?>
+
+//-----------------------------------EXAMPLE PDO---------------------------
+
+<!--
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
+
+-->
